21211d586b773c62dec91396b17daf605fb7b424
Evandro Celino da Silva <celino.evandro@gmail.com>
2026-09-17 14:32:19 +0200

    build.sh without zip binary; spacing

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ set -e
 cd "$(dirname "$0")"
 v=$(python3 -c "import json;d=json.load(open('GameData/SEP-KAS1-Patch/SEP-KAS1-Patch.version'))['VERSION'];print(f\"{d['MAJOR']}.{d['MINOR']}.{d['PATCH']}\")")
 mkdir -p dist && rm -f "dist/SEP-KAS1-Patch-$v.zip"
-zip -qr "dist/SEP-KAS1-Patch-$v.zip" GameData LICENSE README.md
+python3 -c "import shutil,zipfile,os
+z=zipfile.ZipFile('dist/SEP-KAS1-Patch-$v.zip','w',zipfile.ZIP_DEFLATED)
+for root,_,fs in os.walk('GameData'):
+    [z.write(os.path.join(root,f)) for f in fs]
+z.write('LICENSE'); z.write('README.md'); z.close()"
 echo "dist/SEP-KAS1-Patch-$v.zip"
